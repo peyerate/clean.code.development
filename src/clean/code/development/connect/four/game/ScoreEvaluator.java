636fb99994aff265f43ca269ec651458bd437f48
maltewestendorf <maltewestendorf@maltes-mbp.fritz.box>
2017-11-12 16:48:57 +0100

    implemented horizontal evaluation

--- src/clean/code/development/connect/four/game/ScoreEvaluator.java
+++ src/clean/code/development/connect/four/game/ScoreEvaluator.java
@@ -1,18 +1,122 @@
 package clean.code.development.connect.four.game;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class ScoreEvaluator {
 
-	
 	/**
-	 * This method checks if there is currently a winning player.
-	 * That means player cross or dot must have four tiles in a row, stacked, or diagonal.
+	 * This method checks if there is currently a winning player. That means
+	 * player cross or dot must have four tiles in a row, stacked, or diagonal.
 	 * 
 	 * @param field
 	 * @param cross
 	 * @param dot
 	 */
-	public void evaluateGameStatus(Grid field, Player cross, Player dot) {
+	public Player evaluateGameStatus(Grid grid, Player cross, Player dot) {
+//		Map map = createResultList(field, cross);
+//		map.forEach((a,b)->{System.out.println("key: " + a + ", key: " + b);});
+//		System.out.println("");
+		
+		if(checkHorizontalLineForFourInARow(grid, cross)){
+			return cross;
+		} else if(checkHorizontalLineForFourInARow(grid, dot)) {
+			return dot;
+		}
+		return null;
+	}
+
+//	public boolean checkHorizontalLineForFourInARow(Grid field, Player player) {
+//		List<Integer> tmp = new ArrayList<>();
+//		field.getData().forEach(y -> {
+//			if (tmp.size() < 4) {
+//				tmp.clear();
+//			}
+//			y.forEach(x -> {
+//				if (x.equals(player.getTile())) {
+//					tmp.add(1);
+//					if (tmp.size() == 4) {
+//						return;
+//					}
+//				} else {
+//					if(tmp.size()<4) {
+//						tmp.clear();
+//					}
+//				}
+//			});
+//		});
+//		if (tmp.size() >= 4) {
+//			return true;
+//		}
+//		return false;
+//	}
+	
+	public boolean checkHorizontalLineForFourInARow(Grid grid, Player player) {
+		Map<Integer, List<Integer>> result = createResultList(grid, player);
+		for(Map.Entry<Integer, List<Integer>> list : result.entrySet()) {
+			if(checkIfFourNumbersAtLeastAreConsecutiveAccending(list.getValue())) {
+				return true;
+			}
+		}
+		return false;
 		
 	}
 	
+	public boolean checkVerticalLineForFourInARow(Grid grid, Player player) {
+		List<String> results = new ArrayList<>();
+		grid.getData().stream().filter(y -> {
+			
+			return true;
+		}).collect(Collectors.toList());
+		return false;
+	}
+
+	public boolean checkDiagonalLineForFourInARow(Grid grid, Player player) {
+		return false;
+	}
+	
+	public Map<Integer,List<Integer>> createResultList(Grid grid, Player player) {
+		Map<Integer, List<Integer>> results = new HashMap<>();
+		for (int i = 0; i < grid.getData().size(); i++) {
+			for (int j = 0; j < grid.getData().get(i).size(); j++) {
+				if (grid.getData().get(i).get(j).equals(player.getTile())) {
+					if (results.containsKey(i)) {
+						results.get(i).add(j);
+					} else {
+						List<Integer> innerlist = new ArrayList<>();
+						innerlist.add(j);
+						results.put(i, innerlist);
+					}
+				}
+			}
+			
+		}
+		return results;
+	}
+	
+	public boolean checkIfFourNumbersAtLeastAreConsecutiveAccending(List<Integer> list) {
+		Collections.sort(list);
+		int counter = 0;
+		for (int i = 0; i < list.size(); i++) {
+			if (i == 0) {
+				counter++;
+			} else if(i>0) {
+				Integer previousValue = list.get(i-1);
+				Integer currentValue = list.get(i);
+				Integer sum = previousValue + 1;
+				if(sum == currentValue) {
+					counter++;
+				} else {
+					counter = 0;
+				}
+			}
+		}
+		return counter >= 4;
+	}
 }
--- test/clean/code/development/ScoreEvaluatorTest.java
+++ test/clean/code/development/ScoreEvaluatorTest.java
@@ -1,6 +1,9 @@
 package clean.code.development;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -25,27 +28,96 @@ public class ScoreEvaluatorTest {
 	}
 	
 	@Test
-	public void testIf() {
+	public void testIfPlayerCrossHasHorizontalWin() {
 		Player cross = new Player(TileStyle.CROSS.value());
 		Player dot = new Player(TileStyle.DOT.value());
 		field.inserTile(0, cross);
 		field.inserTile(1, cross);
 		field.inserTile(2, cross);
 		field.inserTile(3, cross);
-		
 		field.inserTile(4, dot);
 		field.inserTile(5, dot);
 		field.inserTile(6, dot);
-		
-//		field.getData().get(5).set(0, "[X]");
-//		field.getData().get(5).set(1, "[X]");
-//		field.getData().get(5).set(2, "[X]");
-//		field.getData().get(5).set(3, "[X]");
-//		field.getData().get(5).set(4, "[O]");
-//		field.getData().get(5).set(5, "[O]");
-//		field.getData().get(5).set(6, "[O]");
 		field.printGameField();
-		evaluator.evaluateWinningStatusOfPlayer(field, cross, dot);
-		Assert.assertEquals(2, 2);
+		Assert.assertEquals(evaluator.evaluateGameStatus(field, cross, dot), cross);
+	}
+
+	@Test
+	public void testIfPlayerDotHasHorizontalWin() {
+		Player cross = new Player(TileStyle.CROSS.value());
+		Player dot = new Player(TileStyle.DOT.value());
+		field.inserTile(0, cross);
+		field.inserTile(1, cross);
+		field.inserTile(2, cross);
+		field.inserTile(0, cross);
+		field.inserTile(6, cross);
+		field.inserTile(6, cross);
+		field.inserTile(1, dot);
+		field.inserTile(2, dot);
+		field.inserTile(3, dot);
+		field.inserTile(3, dot);
+		field.inserTile(4, dot);
+		field.inserTile(4, dot);
+		field.inserTile(5, dot);
+//		field.printGameField();
+		Assert.assertEquals(evaluator.evaluateGameStatus(field, cross, dot), dot);
+	}
+	
+	@Test
+	public void testIfPlayerCrossHasVerticalWin() {
+		Player cross = new Player(TileStyle.CROSS.value());
+		Player dot = new Player(TileStyle.DOT.value());
+		field.inserTile(0, cross);
+		field.inserTile(0, cross);
+		field.inserTile(0, cross);
+		field.inserTile(0, cross);
+		
+		field.inserTile(1, dot);
+		field.inserTile(1, dot);
+		field.inserTile(1, dot);
+		
+		Assert.assertEquals(evaluator.evaluateGameStatus(field, cross, dot), cross);
+	}
+	
+	@Test
+	public void testIfPlayerDotHasVerticalWin() {
+		Player cross = new Player(TileStyle.CROSS.value());
+		Player dot = new Player(TileStyle.DOT.value());
+		field.inserTile(0, cross);
+		field.inserTile(0, cross);
+		field.inserTile(0, cross);
+		field.inserTile(1, dot);
+		field.inserTile(1, dot);
+		field.inserTile(1, dot);
+		field.inserTile(1, dot);
+		Assert.assertEquals(evaluator.evaluateGameStatus(field, cross, dot), dot);
+	}
+	
+	@Test
+	public void testIfNumbersAreConsecutiveAccending() {
+		List<Integer> list = new ArrayList<>();
+		list.add(13);
+		list.add(11);
+		list.add(12);
+		list.add(10);
+		Assert.assertEquals(evaluator.checkIfFourNumbersAtLeastAreConsecutiveAccending(list), true);
+	}
+	
+	@Test
+	public void testIfNumbersAreNotConsecutiveAccending() {
+		List<Integer> list = new ArrayList<>();
+		list.add(11);
+		list.add(10);
+		list.add(14);
+		list.add(13);
+		Assert.assertEquals(evaluator.checkIfFourNumbersAtLeastAreConsecutiveAccending(list), false);
+	}
+	
+	@Test
+	public void testIfMethodReturnsFalseIfListIsSmallerThanFour() {
+		List<Integer> list = new ArrayList<>();
+		list.add(11);
+		list.add(12);
+		Assert.assertEquals(evaluator.checkIfFourNumbersAtLeastAreConsecutiveAccending(list), false);
 	}
 }
